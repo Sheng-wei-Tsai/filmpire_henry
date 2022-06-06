381fb38f2fd03c6f5aae096a94fae1cdd1c51830
Henry Tsai <henry88002605@gmail.com>
2022-06-06 16:41:19 +0800

    tested new installation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+const App = () => (
+  <div>
+    <CssBaseline />
+    <h1>Hello, World - Filmpire</h1>
+  </div>
+);
+
+export default App;
