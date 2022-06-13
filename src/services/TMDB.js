4198fde28156b9595607cbd58b720ce6782a0873
Henry Tsai <henry88002605@gmail.com>
2022-06-13 17:38:53 +0800

    add switch genre functionality

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
+
 // /movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
 
@@ -16,7 +16,18 @@ export const tmdbApi = createApi({
 
     // * Get Movies by [Type]
     getMovies: builder.query({
-      query: () =>  `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+      query: ({ genreIdOrCategoryName, page }) => {
+        // Get Movies by Category
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
+        }
+        // Get Movies by Genre
+        if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
+        }
+        // Get Popular Movies
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+      } 
     }),
   }),
 });
