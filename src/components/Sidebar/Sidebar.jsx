28f233166199693e01cf91be07967c60d7af08f7
Henry Tsai <henry88002605@gmail.com>
2022-06-16 15:15:15 +0800

    setup movie information page section1

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -15,18 +15,16 @@ const categories = [
   { label: 'Upcoming', value: 'upcoming' },
 ];
 
-
-
 const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
-const Sidebar = ({ setMobileOpen }) => {
+function Sidebar({ setMobileOpen }) {
   const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
   const theme = useTheme();
   const classes = useStyles();
   const { data, isFetching } = useGetGenresQuery();
   const dispatch = useDispatch();
-  
+
   return (
     <>
       <Link to="/" className={classes.imageLink}>
@@ -40,7 +38,7 @@ const Sidebar = ({ setMobileOpen }) => {
       <List>
         <ListSubheader>Categories</ListSubheader>
         {categories.map(({ label, value }) => (
-          <Link key={value} className={classes.links} to='/'>
+          <Link key={value} className={classes.links} to="/">
             <ListItem onClick={() => dispatch(selectGenreOrCategory(value))} button>
               <ListItemIcon>
                 <img src={genreIcons[label.toLowerCase()]} alt="genreImage" className={classes.genreImages} height={30} />
@@ -59,7 +57,7 @@ const Sidebar = ({ setMobileOpen }) => {
             <CircularProgress />
           </Box>
         ) : data.genres.map(({ name, id }) => (
-          <Link key={name} className={classes.links} to='/'>
+          <Link key={name} className={classes.links} to="/">
             <ListItem onClick={() => dispatch(selectGenreOrCategory(id))} button>
               <ListItemIcon>
                 <img src={genreIcons[name.toLowerCase()]} alt="genreImage" className={classes.genreImages} height={30} />
@@ -71,6 +69,6 @@ const Sidebar = ({ setMobileOpen }) => {
       </List>
     </>
   );
-};
+}
 
 export default Sidebar;
