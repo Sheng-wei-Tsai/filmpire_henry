f2d1b44d61704faf286b0287674caa76e3f7c137
Henry Tsai <henry88002605@gmail.com>
2022-06-23 06:20:46 +0800

    added togglecolor mode

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
 
-import {setUser, userSelector} from '../../features/auth'
+import { ColorModeContext } from '../../utils/ToggleColorMode';
+
+import { setUser, userSelector } from '../../features/auth';
 import { fetchToken, createSessionId, moviesApi } from '../../utils';
 import { Sidebar, Search } from '..';
 import useStyles from './styles';
 
-const NavBar = () => {
+function NavBar() {
   const { isAuthenticated, user } = useSelector(userSelector);
   const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
   const theme = useTheme();
   const dispatch = useDispatch();
-  
+
+  const colorMode = useContext(ColorModeContext);
+
   const token = localStorage.getItem('request_token');
   const sessionIdFromLocalStorage = localStorage.getItem('session_id');
 
@@ -25,24 +29,22 @@ const NavBar = () => {
     const logInUser = async () => {
       if (token) {
         if (sessionIdFromLocalStorage) {
-          
           const { data: userData } = await moviesApi.get(`/account?session_id=${sessionIdFromLocalStorage}`);
           dispatch(setUser(userData));
         } else {
-          
           const sessionId = await createSessionId();
           const { data: userData } = await moviesApi.get(`/account?session_id=${sessionId}`);
           dispatch(setUser(userData));
         }
       }
     };
-     
+
     logInUser();
-  }, [token]) //eslint-disable-next-line;
+  }, [token]); // eslint-disable-next-line;
 
   return (
     <>
-      <AppBar position='fixed'>
+      <AppBar position="fixed">
         <Toolbar className={classes.toolbar}>
           {isMobile && (
             <IconButton
@@ -58,59 +60,58 @@ const NavBar = () => {
           <IconButton
             color="inherit"
             sx={{ ml: 1 }}
-            onClick={() => { }}
+            onClick={colorMode.toggleColorMode}
           >
             {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
-          {!isMobile && <Search/> }
+          {!isMobile && <Search /> }
           <div>
             {!isAuthenticated ? (
               <Button color="inherit" onClick={fetchToken}>
                 Login &nbsp; <AccountCircle />
               </Button>
-            ): (
-                <Button
-                  color='inherit'
-                  component={Link}  
-                  to={`/profile/${user.id}`}
-                  className={classes.linkButton}
-                  onClick={() => {}}
-                >
-                  {!isMobile && <>My Movies &nbsp; </>}
-                  <Avatar
-                    style={{ width: 30, height: 30 }}
-                    alt="Profile"
-                    src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
-                  />
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={`/profile/${user.id}`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp; </>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
               </Button>
             )}
           </div>
-          {isMobile && <Search/>}
+          {isMobile && <Search />}
         </Toolbar>
       </AppBar>
       <div>
         <nav className={classes.drawer}>
           {isMobile ? (
             <Drawer
-              variant='temporary'
-              anchor='right'
+              variant="temporary"
+              anchor="right"
               open={mobileOpen}
               onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
               classes={{ paper: classes.drawerPaper }}
-              ModalProps={{ keepMounted:true}}
+              ModalProps={{ keepMounted: true }}
             >
-              <Sidebar setMobileOpen={ setMobileOpen }/>
+              <Sidebar setMobileOpen={setMobileOpen} />
             </Drawer>
           ) : (
-            <Drawer classes={{paper: classes.drawerPaper}} variant="permanent" open>
-              <Sidebar setMobileOpen={setMobileOpen} />  
+            <Drawer classes={{ paper: classes.drawerPaper }} variant="permanent" open>
+              <Sidebar setMobileOpen={setMobileOpen} />
             </Drawer>
-            )
-          }
+          )}
         </nav>
       </div>
     </>
   );
-};
+}
 
 export default NavBar;
